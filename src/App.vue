2bc0ac10b054821f65159a5b9036b4a688999cfb
SEELE-PC\SEELE <18814884122@sina.cn>
2018-04-04 02:22:47 +0800

    fix: 平衡位

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,49 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <div>
+      <span class="content">请输入一个数组，数组元素之间以逗号分隔</span>
+      <input class="input" v-model="obj">
+    </div>
+    <button class="btn" @click="countPoint">按钮</button>
+    <div>平衡位：{{balancePoint}}</div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      obj: '',
+      balancePoint: null
+    }
+  },
+  methods: {
+    countPoint () {
+      this.balancePoint = null
+      let arr = []
+      let sumRight = null
+      let sumLeft = null
+      arr = JSON.parse(`[${this.obj}]`)
+      if (arr.length > 2) {
+        arr.forEach(i => {
+          sumRight += i
+        })
+        for (let [ index, value ] of new Map(arr.map((value, index) => [ index, value ]))) {
+          sumRight -= value
+          if (sumLeft === sumRight) {
+            this.balancePoint = index
+            return
+          } else {
+            sumLeft += value
+          }
+        }
+      } else {
+        this.balancePoint = -1
+      }
+      this.balancePoint = this.balancePoint ? this.balancePoint : -1
+    }
+  }
 }
 </script>
 
@@ -19,5 +55,33 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.content {
+  margin-right: 50px;
+}
+.input {
+  padding: 5px;
+  width: 300px;
+  font-size: 16px;
+  line-height: 18px;
+  border: 1px solid #bbb;
+  border-radius: 5px;
+}
+.btn {
+  margin-top: 50px;
+  width: 80px;
+  height: 40px;
+  background: #fff;
+  border: 1px solid #bbb;
+  border-radius: 5px;
+  font-size: 20px;
+  cursor: pointer;
+  outline: none;
+}
+.btn:hover {
+  background: #f0f0f0;
+}
+.btn:active {
+  background: #ddd;
 }
 </style>
